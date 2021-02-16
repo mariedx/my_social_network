656dcb06386a9980732b7fecff81b2a2b8da4963
MarieDechaux <marie.dechaux@gmail.com>
2021-02-16 16:24:59 +0100

    update fetch

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -2,21 +2,26 @@
 import React from 'react';
 
 const Register = () => {
-  const data = {
-    username: '',
-    email: '',
-    password: '',
+  const fetchFunction = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: 'marie',
+      email: 'mdmd@gmail.com',
+      password: 'blabla',
+    };
+
+    fetch('http://localhost:1337/auth/local/register', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+    console.log(data);
   };
-
-  fetch('http://localhost:1337/auth/local/register', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-
-  console.log(data);
 
   return (
     <div className="Register">
@@ -24,7 +29,7 @@ const Register = () => {
         <input type="text" name="username" placeholder="Put your username here" />
         <input type="email" name="email" placeholder="email" />
         <input type="password" name="password" placeholder="password" />
-        <button type="submit">ça part</button>
+        <button type="submit" onClick={fetchFunction}>ça part</button>
       </form>
     </div>
   );

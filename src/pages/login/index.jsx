730b55c187d484bbc048cac0fe9124f779e7048d
Clementine <clementine.fouque@gmail.com>
2021-02-17 10:29:09 +0100

    page login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import { React, useState } from 'react';
+import Cookies from 'js-cookie';
 
-const Login = () => (
-  <div className="Login">
-    <h1>Log-In</h1>
-  </div>
-);
+const Login = () => {
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
+
+  const fetchFunction = (e) => {
+    e.preventDefault();
+    const data = {
+      identifier,
+      password,
+    };
+
+    fetch('http://localhost:1337/auth/local', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((jwt) => {
+        Cookies.set('token', jwt);
+        console.log(jwt);
+      });
+  };
+
+  return (
+    <div className="Login">
+      <form>
+        <input type="email" name="email" placeholder="email" value={identifier} onChange={(e) => setIdentifier(e.target.value)} />
+        <input type="password" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" onClick={fetchFunction}>ça part</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;

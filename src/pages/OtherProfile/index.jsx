8d9b3e0bd41002708c9379544f67d0cd908cd65e
MarieDechaux <marie.dechaux@gmail.com>
2021-02-18 14:52:46 +0100

    display user posts

--- src/pages/OtherProfile/index.jsx
+++ src/pages/OtherProfile/index.jsx
@@ -1,12 +1,14 @@
 import { useParams } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { React, useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 const OtherProfile = () => {
   const { userID } = useParams();
-  const [authorInfo, setAuthorInfo] = useState('');
+  const [userInfo, setuserInfo] = useState('');
+  const [userPost, setUserPost] = useState([]);
 
-  const getAuthorInfo = () => {
+  const UserInfo = () => {
     fetch(`http://localhost:1337/users/${userID}`, {
       method: 'get',
       headers: {
@@ -16,17 +18,42 @@ const OtherProfile = () => {
     })
       .then((response) => response.json())
       .then((response) => {
-        setAuthorInfo(response);
+        setuserInfo(response);
       });
   };
 
-  useEffect(() => getAuthorInfo(), []);
+  useEffect(() => UserInfo(), []);
+
+  const UserPost = () => {
+    fetch(`http://localhost:1337/posts?user.id=${userID}`, {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${Cookies.get('token')}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setUserPost(response);
+        console.log(userPost);
+      });
+  };
 
   return (
     <div className="card">
       <h2>Profil de l`auteur:</h2>
-      <p>{authorInfo.username}</p>
-      <p>{authorInfo.email}</p>
+      <p>{userInfo.username}</p>
+      <p>{userInfo.email}</p>
+      <button type="button" onClick={UserPost}>See user posts</button>
+      {userPost.map((post) => (
+        <li key={post.id} style={{ listStyleType: 'none' }}>
+          {post.user.username}
+          <br />
+          {dayjs(post.created_at).format('DD MMMM YYYY')}
+          <br />
+          {post.text}
+        </li>
+      ))}
     </div>
   );
 };
